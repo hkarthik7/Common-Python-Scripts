6b917a5e8f28aaa1e2bea98b5b1be999c448cebb
Harish Karthic <harish.karthic@gmail.com>
2020-02-08 18:50:59 +0000

    Updated python_logging.py script

--- python_logging.py
+++ python_logging.py
@@ -1,26 +1,43 @@
+"""Custom Logger provides the flexibility of logging any python program
+dynamically. This module can be used as it is by instantiating the
+CustomLogger class.
+"""
+
+
 class CustomLogger():
 
     def __init__(self, log_level="WARNING"):
         self.log_level = log_level
-    
-    def start_logging(self, log_level, message):
 
+    def start_logging(self, log_level, message):
+        """Pass the logging level such as INFO, DEBUG
+        and the message that have to be logged.
+        All the progresses are printed in the console
+        and can be divered to a log file.
+        """
         import datetime
 
         self.log_level = log_level
         self.message = message
-        
+
         if self.log_level == None:
             return None
         else:
-            dt = str(datetime.datetime.now()).replace(" ","T")
+            dt = str(datetime.datetime.now()).replace(" ", "T")
             _datetimeformat = f"[{dt}] : "
             print(_datetimeformat + self.log_level + " : " + message)
             return _datetimeformat + self.log_level + " : " + message
 
     def start_json_logging(self, log_level, message):
-
-        import datetime, json
+        """Pass the logging level such as INFO, DEBUG
+        and the message that have to be logged.
+        All the progresses are printed in the console
+        and can be divered to a log file. The logging
+        information are returned and printed in JSON
+        format.
+        """
+        import datetime
+        import json
 
         self.log_level = log_level
         self.message = message
@@ -29,13 +46,14 @@ class CustomLogger():
             return None
         else:
             logs = {
-                "Level" : log_level,
-                "DateTime" : str(datetime.datetime.now()),
-                "Message" : message
+                "Level": log_level,
+                "DateTime": str(datetime.datetime.now()).replace(" ", "T"),
+                "Message": message
             }
             log_json = json.dumps(logs)
             print(log_json)
             return log_json
+
 
 my_log = CustomLogger()
 my_log.start_logging("INFO", "This is an information message!")
